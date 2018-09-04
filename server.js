95f79fd79615c373dc4a8418b065e10c1262248c
unknown <1511020010067@PL15.swe.edu>
2018-09-04 21:24:32 +0500

    login page added

--- server.js
+++ server.js
@@ -3,17 +3,7 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.get('/', (req, res)=>{
-    res.send('index.html');
-});
 
-app.get('/home', (req, res)=>{
-    res.send('index.html');
-});
-
-// app.get('/register', (req, res)=>{
-//     res.send('register.html');
-// });
 
 
 
